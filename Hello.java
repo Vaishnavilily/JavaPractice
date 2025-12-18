74c27bde8713796679d8f4389cbc7f999053e9cb
Vaishnavi Vadlamani <110436750+Vaishnavilily@users.noreply.github.com>
2025-12-18 19:08:59 +0530

    Add Hello.java with introductory message

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+  public static void main(String[] args){
+    System.out.println("Hello, I'm Vivi!");
+    System.out.println("I'm a Btech CSE final year Student who started learning java again from scratch!");
+  }
+}
